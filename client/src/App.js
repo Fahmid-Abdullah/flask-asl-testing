21d10ed990953a4dac804a960c47e8f13295883b
Fahmid Abdullah <fahmidabdullah@hotmail.com>
2024-02-20 09:43:55 -0500

    Files

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://127.0.0.1:5000';  // Update with your Flask server endpoint
+
+const App = () => {
+  const [predictionText, setPredictionText] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+  
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  
+    socket.on('sentence', data => {
+      console.log('Received sentence:', data.sentence);
+      setPredictionText(data.sentence.toString());
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+  
+    return () => {
+      socket.disconnect();
+    };
+  }, []);  
+
+  return (
+    <div>
+      <img src="http://127.0.0.1:5000/video_feed" alt="Prediction" />
+      <pre>{predictionText}</pre>
+    </div>
+  );
+};
+
+export default App;
